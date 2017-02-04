7422f6a553693835ee8e6336596667a04838b312
connectedblue <connectedblue@users.noreply.github.com>
2017-02-04 10:04:08 +0000

    checkpoint when restarting project in Feb 2017

--- capstone/src/ui.R
+++ capstone/src/ui.R
@@ -7,27 +7,21 @@ library(plotly)
 # UI for the map display
 
 shinyUI(pageWithSidebar(
-        headerPanel("Bristol City traffic"),
+        headerPanel("Next Word Predictor"),
         sidebarPanel(
-                p('Select below to explore average traffic conditions on that day and hour'),
-                selectInput("day", "Day", 
-                            choices=c("Sunday", "Monday", "Tuesday", "Wednesday",
-                                      "Thursday", "Friday", "Saturday"),
-                            selected = "Monday"),
-                sliderInput("hour", "Hour", min=0, max=23,
-                            value = 9, step = 1)
+                p('Select the number of predictions for next word'),
+               
+                sliderInput("words", "Number", min=1, max=5,
+                            value = 3, step = 1)
                 
         ),
         mainPanel(
                 tabsetPanel(
-                        tabPanel("Traffic Map", 
+                        tabPanel("Word Predictor", 
                                  p("The circles are the centre of each route.
                                    Click to show detailed route information for this time"),
-                                 leafletOutput("map")),
-                        tabPanel("Route Speeds",
-                                 p("The bar graph below shows the average speed of each route
-                                   during this time period. Hover over each bar to see the route name"),
-                                 plotlyOutput("plot")),
+                                 leafletOutput("map")
+                                 ),
                         tabPanel("About", includeHTML("instructions.html"))
                         
                 )
